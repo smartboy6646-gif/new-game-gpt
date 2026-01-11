034a50a134acb72b33db1c26c3b9af4ceae848c8
smartboy6646-gif <smartboy6646@gmail.com>
2026-01-11 16:52:33 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
   storageBucket: "call-breke.firebasestorage.app",
   messagingSenderId: "908751603046",
   appId: "1:908751603046:web:fd30e07b9e6400b35c74a7",
-  measurementId: "G-V2D55S47QV
+  measurementId: "G-V2D55S47QV"
 };
 
 firebase.initializeApp(firebaseConfig);
